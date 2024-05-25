086319c94612dab3dfa4f819cc571219212b5a34
abhisek1221 <cherrymx317@gmail.com>
2024-05-25 12:54:48 +0530

    Property List Server Actions

--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {SelectedCategory} from "../../../../components/SelectedCategory";
+import Link from 'next/link';
 
 export default function StructureRoute() {
     return (
@@ -14,7 +15,11 @@ export default function StructureRoute() {
         <SelectedCategory />
         <div className="fixed w-full bottom-0 z-10 bg-white border-t h-24">
           <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
-            <Button variant={"secondary"} size="lg">Cancel</Button>
+            <Button variant={"secondary"} size="lg" asChild>
+              <Link href='/'>
+              Cancel
+              </Link>
+              </Button>
             <Button size="lg">Save</Button>
           </div>
         </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../public/1.png"
 import UserNav from "./UserNav";
+import {Button} from "@/components/ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus, faAddressBook } from "@fortawesome/free-solid-svg-icons";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { listProperty } from "@/app/actions";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const {getUser} = getKindeServerSession();
+    const user = await getUser()
+  const listPropertywithId = listProperty.bind(null, {
+    userId : user?.id as string
+  });
   return (
     <nav className="w-full border-b">
       <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
@@ -19,7 +29,24 @@ export default function Navbar() {
             className="block lg:hidden w-12"
           />
         </Link>
+        <div className="flex items-center">
+          <form action={listPropertywithId}>
+          <Button className="mr-2 bg-purple-300 px-4 flex items-center" variant="outline" type="submit">
+          <FontAwesomeIcon icon={faCirclePlus} height={30} width={30} />
+            List Property
+        </Button>
+          </form>
+        
+        <Button className="mr-2">
+        <FontAwesomeIcon icon={faAddressBook} height={30} width={30} />
+          <Link href="/create">
+            Contact
+          </Link>
+        </Button>
         <UserNav />
+        </div>
+
+       
       </div>
     </nav>
   );
